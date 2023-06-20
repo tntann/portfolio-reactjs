6d9a8fd3585adbdaa8d1804e36bec35c33357b5b
Nhat Tan <nhattan8421@gmail.com>
2023-06-20 23:36:27 +0700

    portfolio-reactjs

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+    es2022: true,
+    amd: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+  ],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "18.2" } },
+  plugins: ["react-refresh", "react"],
+  rules: {
+    "react-refresh/only-export-components": "warn",
+    "react/jsx-uses-vars": "error",
+    "react/jsx-uses-react": "error",
+    "react/prop-types": "off",
+    "no-case-declarations": "off",
+  },
+};
