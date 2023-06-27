beb9a2b55ba11f28f1a129e66871d1b17c0fd39c
Nhat Tan <nhattan8421@gmail.com>
2023-06-27 17:42:05 +0700

    portfolio-reactjs

--- src/components/project_card/ProjectCard.jsx
+++ src/components/project_card/ProjectCard.jsx
@@ -8,8 +8,12 @@ const ProjectCard = (props) => {
         className="border mx-auto w-[90%] md:w-[48%] lg:w-[30%] border-[#ABB2BF] p-0"
       >
         {/* img */}
-        <div className="">
-          <img className="w-full object-cover" src={props.img} alt="" />
+        <div className="relative overflow-hidden bg-cover bg-no-repeat">
+          <img
+            className="w-full object-cover transition duration-300 ease-in-out hover:scale-125 cursor-zoom-in"
+            src={props.img}
+            alt=""
+          />
         </div>
         {/* skill */}
         <div className="flex gap-2 flex-wrap p-2 border-y border-[#ABB2BF]">
